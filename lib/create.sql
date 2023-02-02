89e96c6111fdf815aacf7717adb54af0456b7ca1
ron <rvpostha15@yahoo.com>
2023-02-02 17:50:14 -0500

    commit tree a174c8f49c767770b7b43cafc9f07eaec6a26bea

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    -- booleans => 0 = false, 1 = true
+    alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'happy', 1);
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 12, 'M', 'red', 'sad', 1);
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 12, 'F', 'orange', 'stern', 0);
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'yellow', 'sleepy', 1);
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) VALUES ('Melissa', 12, 'F', 'green', 'mad', 0);
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) VALUES ('Grinch', 12, 'M', 'blue', 'happy', 1);
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'indigo', 'curious', 0);
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) VALUES (NULL, 12, 'F', 'violet', 'happy', 1);
